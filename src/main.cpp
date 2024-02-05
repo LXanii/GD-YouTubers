d54c55243cabd4bd010d229807f0247bc8cc2d17
Xanii <73562093+LXanii@users.noreply.github.com>
2024-02-05 16:34:06 -0500

    fix !res crash

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/ProfilePage.hpp>
 #include <Geode/utils/web.hpp>
+#include <cctype>
 
 using namespace geode::prelude;
 bool downloaded = false;
@@ -21,22 +22,32 @@ CCSprite * icon;
 			if (!res) {
 				log::info("Failed to fetch file from GitHub.");
 			}
-			auto data = res.value();
+			else {
+				auto data = res.value();
         
-			std::istringstream iss(data);
-			std::string temp_string;
-			log::info("Downloaded YouTuber List");
+				std::istringstream iss(data);
+				std::string temp_string;
+				log::info("Downloaded YouTuber List");
 
-			while (iss >> temp_string) {
-				YouTubers.insert(temp_string);
+				while (iss >> temp_string) {
+					YouTubers.insert(temp_string);
+				}
+				downloaded = true;
 			}
-			downloaded = true;
 		}
 
 		std::string player_name = m_usernameLabel->getString();
 
 		for (const auto& names : YouTubers) {
-			if (names == player_name) {
+			std::string lower_names(names.begin(), names.end());
+			std::string lower_player_name(player_name.begin(), player_name.end());
+			for (char &c : lower_names) {
+				c = std::tolower(c);
+			}
+			for (char &c : player_name) {
+				c = std::tolower(c);
+			}
+			if (lower_names == lower_player_name) {
 				log::info("YouTuber Found! {}", names);
 				CCNode* first_letter = reinterpret_cast<CCNode*>(m_usernameLabel->getChildren()->objectAtIndex(0));
 				m_fields->icon = CCSprite::create("youtuber.png"_spr);
